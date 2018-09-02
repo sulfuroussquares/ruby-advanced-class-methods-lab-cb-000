027382aa4d2787996a3fb634597234c867c2cb88
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-09-02 23:00:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,50 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    #song.name = name
+    @@all << song
+    song
+  end
+
+def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  @@all << song
+  song
+end
+
+def self.create_by_name(name)
+  song = self.new
+  song.name = name
+  @@all << song
+  song
+end
+
+def self.find_by_name(name)
+  self.all.detect{|song| song.name == name}
+end
+
+def self.find_or_create_by_name(name)
+  if (self.find_by_name(name) == nil)
+    self.create_by_name(name)
+  else
+    self.find_by_name(name)
+  end
+
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+def self.new_from_filename(filename)
+  song = self.new
+  song.name = (name.split(" - ")[1].chomp(".mp3"))
+  song.artist_name = (name.split(" - ")[0])
+  song
+end
+
+end
+
 end
